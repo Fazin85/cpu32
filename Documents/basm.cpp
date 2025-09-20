954540fdb8b0d1dccea4940d8acda9eb5afa6efb
Fazin85 <grahamderson1@gmail.com>
2025-09-19 20:30:49 -0400

    more yaya

--- Documents/basm.cpp
+++ Documents/basm.cpp
@@ -42,35 +42,45 @@ void gi(uint8_t o, uint8_t r1, uint8_t r2, uint16_t i) {
 #define NOP 0
 #define ADD 1
 #define SUB 2
-#define LOAD 3
-#define STORE 4
+#define LW 3
+#define SW 4
 #define LOADI 5
 #define JNZ 6
 #define ADC 7
 #define SBB 8
 #define LUI 9
 #define ORI 10
+#define LH 11
+#define LHU 12
+#define LB 13
+#define LBU 14
+#define SH 15
+#define SB 16
 
 int main() {
-  gi(LOADI, 0, 0, 6);  // LOADI R0, #6
-  gi(LOADI, 1, 0, 67); // LOADI R1, #67
+  gi(LOADI, 1, 0, 6);  // LOADI R1, #6
+  gi(LOADI, 2, 0, 67); // LOADI R2, #67
 
   // Add them together (result goes to R0)
-  gi(ADD, 0, 1, 6); // ADD R0, R1    (R0 = R0 + R1 = 6 + 67 = 73)
-
-  // Load address 2 into a register for storing
-  gi(LOADI, 2, 0, 2); // LOADI R2, #2
+  gi(ADD, 1, 2, 6); // ADD R1, R2, 6    (R0 = R0 + R1 = 6 + 6 + 67 = 79)
 
   // Store the result to address 2
-  gi(STORE, 2, 0, 0); // STORE [R2], R0
-  gi(LOADI, 1, 0, 15);
+  gi(SW, 1, 0, 4); // STORE [R0], R1, 4
   gi(LOADI, 2, 0, 15);
-  gi(SUB, 1, 2, 0);
+  gi(LOADI, 3, 0, 15);
+  gi(SUB, 2, 3, 0);
 
+  gi(LW, 13, 0, 4);
   gi(JNZ, 0, 0, 0);
 
-  gi(LUI, 0, 0, 0x1234); // R0 = 0x12340000
-  gi(ORI, 0, 0, 0x5678); // R0 = 0x12345678
-
+  // gi(LUI, 1, 1, 0x1234); // R0 = 0x12340000
+  // gi(ORI, 1, 1, 0x5678); // R0 = 0x12345678
+  gi(LUI, 1, 1, 0x7fff);
+  gi(ORI, 1, 1, 0xffff);
+  gi(SW, 1, 0, 4);
+  gi(LW, 5, 0, 4);
+  gi(LBU, 2, 0, 4);
+  gi(LBU, 3, 0, 5);
+  gi(LHU, 4, 0, 6);
   writeLogisimInstructions("program.rom", instructions);
 }
